3ec36b04201475a658a282ac6970670174b35af4
vexxonsult <vexconsult.fr@gmail.com>
2026-08-30 15:28:44 +0200

    Lire les JO L depuis la vue quotidienne EUR-Lex

--- tests/test_collect_eurlex_oj.py
+++ tests/test_collect_eurlex_oj.py
@@ -1,21 +1,22 @@
+import datetime as dt
 import unittest
 
-from scripts.collect_eurlex_oj import build_query, records_from_response
+from scripts.collect_eurlex_oj import daily_view_url, records_from_html
 
 
 class EurlexCollectorTests(unittest.TestCase):
-    def test_query_is_bounded_by_the_requested_dates(self):
-        query = build_query("2026-08-19", "2026-08-29")
-        self.assertIn('"2026-08-19"^^xsd:date', query)
-        self.assertIn('"2026-08-29"^^xsd:date', query)
-        self.assertIn("cdm:official-journal_class", query)
+    def test_builds_the_official_daily_view_for_a_date(self):
+        url = daily_view_url(dt.date(2026, 8, 21))
+        self.assertIn("ojDate=21082026", url)
+        self.assertIn("daily-view/L-series", url)
 
-    def test_keeps_only_l_series_and_preserves_raw_metadata(self):
-        response = {"results": {"bindings": [
-            {"uri": {"value": "http://publications.europa.eu/resource/cellar/l"}, "ojclass": {"value": "http://publications.europa.eu/resource/authority/oj-type/L"}, "ojnumber": {"value": "200"}, "ojcollection": {"value": "OJ"}, "ojyear": {"value": "2026"}, "workdatedoc": {"value": "2026-08-29"}},
-            {"uri": {"value": "http://publications.europa.eu/resource/cellar/c"}, "ojclass": {"value": "http://publications.europa.eu/resource/authority/oj-type/C"}, "ojnumber": {"value": "201"}, "ojcollection": {"value": "OJ"}, "ojyear": {"value": "2026"}, "workdatedoc": {"value": "2026-08-29"}},
-        ]}}
-        records = records_from_response(response)
+    def test_extracts_l_series_acts_without_interpretation(self):
+        page = '''<html><body>
+          <a href="./../../../legal-content/EN/TXT/?uri=OJ:L_202601960">Commission Implementing Regulation (EU) 2026/1960</a>
+          <a href="./../../../legal-content/EN/TXT/?uri=OJ:C_202600001">Information notice</a>
+        </body></html>'''
+        records = records_from_html(page, "https://eur-lex.europa.eu/oj/daily-view/L-series/default.html?ojDate=21082026", "2026-08-21")
         self.assertEqual(len(records), 1)
-        self.assertEqual(records[0]["official_journal_number"], "200")
+        self.assertEqual(records[0]["official_journal_id"], "OJ:L_202601960")
+        self.assertEqual(records[0]["title"], "Commission Implementing Regulation (EU) 2026/1960")
         self.assertIsNone(records[0]["interpretation"])
